f20ed74375395e3a6117fc2cc584860857d02a0d
Susumu Yamazaki <zacky1972@gmail.com>
2017-12-23 04:03:29 +0900

    downgrade jpeg quality

--- config.rb
+++ config.rb
@@ -459,7 +459,7 @@ configure :build do
     # false instead of a hash
     options.advpng    = { :level => 4 }
     options.gifsicle  = { :interlace => false }
-    options.jpegoptim = { :strip => ['all'], :max_quality => 100 }
+    options.jpegoptim = { :strip => ['all'], :max_quality => 80 }
     options.jpegtran  = { :copy_chunks => false, :progressive => true, :jpegrescan => true }
     options.optipng   = { :level => 6, :interlace => false }
     options.pngcrush  = { :chunks => ['alla'], :fix => false, :brute => false }
