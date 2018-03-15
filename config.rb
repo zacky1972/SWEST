b5ea4d8e301a0423206c3d347c1d2fe6a51844da
Susumu Yamazaki <zacky1972@gmail.com>
2018-03-15 16:26:45 +0900

    create new favicon

--- config.rb
+++ config.rb
@@ -708,7 +708,7 @@ configure :build do
   activate :favicon_maker do |f|
     f.template_dir  = 'source/images'
     f.icons = {
-      "swestlogo.png" => [
+      "SWEST_favicon.svg" => [
         { icon: "apple-touch-icon-180x180-precomposed.png" },
         { icon: "apple-touch-icon-152x152-precomposed.png" },
         { icon: "apple-touch-icon-144x144-precomposed.png" },
