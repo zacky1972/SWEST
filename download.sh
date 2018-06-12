55459c44763e083f06c0a3dd0443dc57dde881f6
Susumu Yamazaki <zacky1972@gmail.com>
2018-06-12 15:04:41 +0900

    update download.sh for SWEST20

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
-rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST19/lecture program-data/SWEST19/
-rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST19/program/interactive-printable.csv program-data/SWEST19/
+#rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST19/lecture program-data/SWEST19/
+#rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST19/program/interactive-printable.csv program-data/SWEST19/
+rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST20/lecture program-data/SWEST20/
 yarn gulp pre
