e0a20fde101a111ffa80e3e85ea86e9faada3254
Susumu Yamazaki <zacky1972@gmail.com>
2018-10-04 06:16:30 +0900

    change encodings of all minutes files

--- download.sh
+++ download.sh
@@ -2,3 +2,4 @@
 #rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST19/program/interactive-printable.csv program-data/SWEST19/
 rsync -auzv -e "ssh -p 60022" swest.toppers.jp:/var/www/html/SWEST20/lecture program-data/SWEST20/
 yarn gulp pre
+find program-data/*/lecture/data/*.txt | xargs -n 10 nkf -w --overwrite
