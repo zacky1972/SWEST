10a16c3bdb7bc7d8abe38b1e848c4a99dba0407d
InaishiHinako <hinako1714@ertl.jp>
2019-07-26 08:53:45 -0700

    change_interactive

--- config.rb
+++ config.rb
@@ -3,12 +3,12 @@ require 'slim'
 require 'csv'
 require 'redcarpet'
 
-preview = false
+preview = true
 
 # allOptions = preview
 allOptions = false
 
-defaultOptions = "p3-s2-i2-r1-R1-S0"
+defaultOptions = "p3-s2-i1-r1-R1-S0"
 
 buildBeforeDeploy = true
 
